6057609484cf6a149311a418a96506c8e876bfea
BetaKrankri <edominguezj11@gmail.com>
2023-01-13 12:28:38 -0600

    Layout the App container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
+import '../borderers.css';
+
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import QuoteBox from '../QuoteBox/QuoteBox';
+
+
 function App() {
+
+
   return (
-    <div className="App">
-    </div>
+    <Container className='App' fluid style={{height: '100vh'}}>
+      <Row className=' justify-content-center align-items-center' style={{height: '100%'}} >
+        <Col sm={6} md={4} xl={3}>
+          <QuoteBox/>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
